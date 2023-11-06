a591ea110b1d690586afb9a3083d03772b877363
Chelsea Katsidzira <1133280@iheartmedia.com>
2023-11-05 23:19:34 -0500

    delete unnecessary classes

--- app/src/main/java/com/example/moviemania/data/dtos/ApiGenresResultDto.kt
+++
@@ -1,5 +0,0 @@
-package com.example.moviemania.data.dtos
-
-data class ApiGenresResultDto<T>(
-    val genres: List<T>
-)
--- app/src/main/java/com/example/moviemania/data/dtos/ApiListResultDto.kt
+++
@@ -1,5 +0,0 @@
-package com.example.moviemania.data.dtos
-
-data class ApiListResultDto<T>(
-    val results: List<T>
-)
