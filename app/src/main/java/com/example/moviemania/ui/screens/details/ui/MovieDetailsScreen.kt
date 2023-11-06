222eba1a4d20603e5ef01132e0ed4619103dc0d0
Chelsea Katsidzira <1133280@iheartmedia.com>
2023-11-06 14:23:45 -0500

    design font

--- app/src/main/java/com/example/moviemania/ui/screens/details/ui/MovieDetailsScreen.kt
+++ app/src/main/java/com/example/moviemania/ui/screens/details/ui/MovieDetailsScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -23,7 +24,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.moviemania.R
@@ -84,41 +88,66 @@ fun MovieDetailsLayout(
                     MovieManiaLargeCard(movie = movie)
                     Text(
                         text = movie.title,
-                        fontSize = dimensionResource(id = R.dimen.details_title_text_size).value.sp,
-                        color = Color.Black,
-                        modifier = Modifier.fillMaxWidth()
+                        style = TextStyle(
+                            fontSize = dimensionResource(id = R.dimen.details_title_text_size).value.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = MaterialTheme.colorScheme.onBackground,
+                        ),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 12.dp)
                     )
                     Text(
                         text = movie.tagline,
-                        fontSize = 12.sp,
-                        color = Color.Black,
-                        modifier = Modifier.fillMaxWidth()
+                        style = TextStyle(
+                            fontSize = 20.sp,
+                            color = MaterialTheme.colorScheme.onBackground,
+                        ),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 12.dp)
+                    )
+                    Text(
+                        text = state.movie.overview,
+                        style = TextStyle(
+                            fontSize = 20.sp,
+                            color = MaterialTheme.colorScheme.onBackground,
+                        ),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 12.dp)
                     )
                     state.movie.genres?.map { it.name }?.let { genres ->
                         Text(
                             text = genres.joinToString(", "),
-                            fontSize = 12.sp,
-                            color = Color.Black,
-                            modifier = Modifier.fillMaxWidth()
+                            style = TextStyle(
+                                fontSize = 16.sp,
+                                color = MaterialTheme.colorScheme.onBackground,
+                            ),
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(bottom = 12.dp)
                         )
                     }
                     Text(
-                        text = state.movie.runtime.toString(),
-                        fontSize = 12.sp,
-                        color = Color.Black,
-                        modifier = Modifier.fillMaxWidth()
+                        text = "Runtime: ${state.movie.runtime.toString()}",
+                        style = TextStyle(
+                            fontSize = 16.sp,
+                            color = MaterialTheme.colorScheme.onBackground,
+                        ),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 12.dp)
                     )
                     Text(
-                        text = state.movie.releaseDate,
-                        fontSize = 12.sp,
-                        color = Color.Black,
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                    Text(
-                        text = state.movie.overview,
-                        fontSize = 12.sp,
-                        color = Color.Black,
-                        modifier = Modifier.fillMaxWidth()
+                        text = "Released: ${state.movie.releaseDate}",
+                        style = TextStyle(
+                            fontSize = 16.sp,
+                            color = MaterialTheme.colorScheme.onBackground,
+                        ),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 12.dp)
                     )
                 }
             }
